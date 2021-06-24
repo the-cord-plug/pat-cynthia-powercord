2a0033bdc2a6edd0ddefca0e202fbe1290fb2e7e
jai 3 <47060357+jdadonut@users.noreply.github.com>
2021-06-24 18:44:45 -0400

    pat

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const { Plugin } = require('powercord/entities');
-
+const { post } = require("powercord/http");
+let x = false;
 module.exports = class Template extends Plugin {
     startPlugin() {
-        return;
+        powercord.api.commands.registerCommand({
+            command: 'pat-cynthia',
+            description: 'pat cynthia!',
+            usage: '{c}',
+            executor: async () => {
+                if (x) {return {send:false, result:"you're being rate limited! (dont pat too fast! only once per 5 seconds)"}}
+
+                let res = await post("https://cynthia.rest/action/pat").execute();
+                x = true;
+                setTimeout(5000, ()=>x=false);
+                return {send: false, result: res.body.message}
+            }
+        })
     }
     pluginWillUnload() {
-        return;
+        powercord.api.commands.unregisterCommand("pat-cynthia");
     }
 };
