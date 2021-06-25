a2c7ffaeeaac831545f13b12fc0f7ed8463b930a
jai 3 <47060357+jdadonut@users.noreply.github.com>
2021-06-25 14:55:30 -0400

    qol fix

--- index.js
+++ index.js
@@ -9,12 +9,13 @@ module.exports = class PatCynthia extends Plugin {
             description: 'pat cynthia!',
             usage: '{c}',
             executor: async () => {
-                if (cantPatCynthia) {return {send:false, result:"you're being rate limited! (dont pat too fast! only once per 5 seconds)"}}
-
-                let res = await post("https://cynthia.rest/action/pat").execute();
-                cantPatCynthia = true;
-                setTimeout(()=>{cantPatCynthia=false}, 5000);
-                return {send: false, result: res.body.message}
+                if (cantPatCynthia) { return { send: false, result: "you're being rate limited! (dont pat too fast! only once per 5 seconds)" } }
+                try {
+                    let res = await post("https://cynthia.rest/action/pat").execute();
+                    cantPatCynthia = true;
+                    setTimeout(() => { cantPatCynthia = false }, 5000);
+                    return { send: false, result: res.body.message }
+                } catch { return { send: false, result: "Error! (You're likely being rate limited, dont hug too fast! (once per 5 seconds))" }; }
             }
         })
         powercord.api.commands.registerCommand({
@@ -22,12 +23,14 @@ module.exports = class PatCynthia extends Plugin {
             description: 'hug cynthia!',
             usage: '{c}',
             executor: async () => {
-                if (cantHugCynthia) {return {send:false, result:"you're being rate limited! (dont hug too fast! only once per hour)"}}
-
-                let res = await post("https://cynthia.rest/action/hug").execute();
-                cantHugCynthia = true;
-                setTimeout(()=>{cantHugCynthia=false}, 3600e3);
-                return {send: false, result: res.body.message}
+                if (cantHugCynthia) { return { send: false, result: "you're being rate limited! (dont hug too fast! only once per hour)" } }
+                try {
+                    let res = await post("https://cynthia.rest/action/hug").execute();
+                    cantHugCynthia = true;
+                    setTimeout(() => { cantHugCynthia = false }, 3600e3);
+                    return { send: false, result: res.body.message }
+                }
+                catch { return { send: false, result: "Error! (You're likely being rate limited, dont hug too fast! (once per hour))" }; }
             }
         })
     }
